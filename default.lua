0ebd67dd631df2c6925f65c902453c82487802f6
Christoph Giesel <mail@cgiesel.de>
2012-08-28 10:13:07 +0200

    lqfb config updated to v2

--- default.lua
+++ default.lua
@@ -1,11 +1,10 @@
 config.app_name = "LiquidFeedback"
-config.app_version = "beta33"
 
 config.absolute_base_url = "http://lqfb.piraten-lsa.de/" .. config.instance_prefix .. "/"
 
-config.app_title = config.app_name .. " der " .. config.instance_name
+-- config.app_title = config.app_name .. " der " .. config.instance_name
 
-config.app_logo = "logo.png"
+-- config.app_logo = "logo.png"
 
 config.app_service_provider = "Piratenpartei Sachsen-Anhalt<br />Pflugstr. 9a<br />10115 Berlin<br /><br /><br \>E-Mail Administratoren: <a href='mailto:it@piraten-lsa.de'>it@piraten-lsa.de</a><br /><br />Weitere Informationen & FAQ: <a href='http://wiki.piratenpartei.de/LQPP'>http://wiki.piratenpartei.de/LQPP</a>"
 
@@ -21,17 +20,6 @@ config.use_terms_checkboxes = {
 }
 
 
-config.member_image_content_type = "image/jpeg"
-config.member_image_convert_func = {
-  avatar = function(data) return os.pfilter(data, "convert", "jpeg:-", "-thumbnail",   "48x48", "jpeg:-") end,
-  photo =  function(data) return os.pfilter(data, "convert", "jpeg:-", "-thumbnail", "240x240", "jpeg:-") end
-}
-
-config.member_image_default_file = {
-  avatar = "avatar.jpg",
-  photo = nil
-}
-
 config.formatting_engine_executeables = {
   rocketwiki= "/opt/rocketwiki-lqfb/rocketwiki-lqfb",
   compat = "/opt/rocketwiki-lqfb/rocketwiki-lqfb-compat"
@@ -43,74 +31,20 @@ config.default_lang = "de"
 -- notation is according to postgresql intervals
 config.delegation_warning_time = '6 months'
 
-config.mail_subject_prefix = "[LiquidFeedback] "
-config.mail_from = { name = "lqfb.piraten-lsa.de", address = "noreply@lqfb.piraten-lsa.de" }
+config.mail_envelope_from = "noreply@lqfb.piraten-lsa.de"
+config.mail_from = { name = "LiquidFeedback - Piratenpartei Sachsen-Anhalt", address = "noreply@lqfb.piraten-lsa.de" }
+config.mail_reply_to = "lqfb@piraten-lsa.de"
 
 config.fastpath_url_func = function(member_id, image_type)
   return config.absolute_base_url .. "fastpath/getpic?" .. tostring(member_id) .. "+" .. tostring(image_type)
 end
 
-config.download_dir = nil
+config.public_access = "none"  -- Available options: "none", "anonymous", "pseudonym", "authors_pseudonymous", "all_pseudonymous", "everything"
 
-config.download_use_terms = "=== Nutzungsbedingungen ===\nAlles ist verboten"
-
-config.public_access = false  -- Available options: "anonymous", "pseudonym"
-
-config.api_enabled = true
-
-config.feature_rss_enabled = false -- feature is broken
-
--- OpenID authentication is not fully implemented yet, DO NOT USE BEFORE THIS NOTICE HAS BEEN REMOVED!
-config.auth_openid_enabled = false
-config.auth_openid_https_as_default = true
-config.auth_openid_identifier_check_func = function(uri) return false end
-
-request.set_allowed_json_request_slots{ "title", "actions", "support", "default", "trace", "system_error" }
-
-
-if request.get_json_request_slots() then
-  request.force_absolute_baseurl()
-end
-
-request.set_404_route{ module = 'index', view = '404' }
-
--- uncomment the following two lines to use C implementations of chosen
--- functions and to disable garbage collection during the request, to
--- increase speed:
---
 -- require 'webmcp_accelerator'
--- collectgarbage("stop")
+-- if cgi then collectgarbage("stop") end
 
--- open and set default database handle
-db = assert(mondelefant.connect{
-  engine='postgresql',
-  dbname='liquid_feedback_' .. config.instance_prefix
-})
-at_exit(function() 
-  db:close()
-end)
-function mondelefant.class_prototype:get_db_conn() return db end
-
--- enable output of SQL commands in trace system
-function db:sql_tracer(command)
-  return function(error_info)
-    local error_info = error_info or {}
-    trace.sql{ command = command, error_position = error_info.position }
-  end
-end
-
-request.set_absolute_baseurl(config.absolute_base_url)
-
-
-
--- TODO abstraction
--- get record by id
-function mondelefant.class_prototype:by_id(id)
-  local selector = self:new_selector()
-  selector:add_where{ 'id = ?', id }
-  selector:optional_object_mode()
-  return selector:exec()
-end
+config.database = { engine='postgresql', dbname='liquid_feedback_' .. config.instance_prefix }
 
 config.footer_html = "<table style=\"width:100%;font-weight:bold;font-size: 90%; margin-top: 8px;\">\
   <colgroup>\
@@ -124,18 +58,21 @@ config.footer_html = "<table style=\"width:100%;font-weight:bold;font-size: 90%;
       <td colspan=\"4\" style=\"padding-left:1em;padding-right:1em;\"><hr></td>\
     </tr>\
     <tr>\
-      <td style=\"padding-left:1.4em;font-size: 90%;\">\
+      <td style=\"font-size: 90%;text-align: center;\">\
         <a href=\"" .. config.absolute_base_url .. "index/usage_terms.html\">Nutzungsbedingungen</a>\
       </td>\
-      <td style=\"padding-left:1.4em;font-size: 90%;\">\
+      <td style=\"font-size: 90%;text-align: center;\">\
         <a href=\"" .. config.absolute_base_url .. "static/doc/pseudonyme.html\">Hinweise zum Umgang mit Pseudonymen</a>\
       </td>\
-      <td align=\"center\" style=\"font-size: 90%;\">\
+      <td style=\"font-size: 90%;text-align: center;\">\
         <a href=\"" .. config.mitgliedwerden .. "\">Mitglied werden</a>\
       </td>\
-      <td align=\"right\" style=\"padding-right:1.4em;font-size: 90%;\">\
+      <td style=\"font-size: 90%;text-align: center;\">\
         <a href=\"" .. config.absolute_base_url .. "index/about.html\">Impressum</a>\
       </td>\
     </tr>\
   </tbody>\
 </table>"
+
+
+execute.config("init")
--- piraten-lsa.lua
+++ piraten-lsa.lua
@@ -7,8 +7,6 @@ execute.config("default")
 
 config.use_terms = "=== Nutzungsbedingungen ===\nFür die Nutzung von LiquidFeedback, nachfolgend System genannt, unter der Domain lqfb.piraten-lsa.de gelten die Nutzungsbedingungen des Landesverbandes Sachsen-Anhalt der Piratenpartei Deutschland. Jedem Pirat, der das System nutzt, sind diese bekannt; er erklärt sich durch Login in das System unter vorgenannter Domain mit ihnen einverstanden.\n\n# Jeder LSA-Pirat, Nutzer genannt, erhält einen Zugang zum System. \n# Jeder Nutzer wird hiermit ausdrücklich darauf hingewiesen, dass alle Einträge und Einstellungen, die er vornimmt, aus Gründen der Nachvollziehbarkeit (Transparenz) für unbegrenzte Zeit im System erhalten bleiben und auch veröffentlicht werden können. Dies gilt auch im Fall des Parteiaustritts.\n# Der Nutzer kann eine Pseudonymisierung seiner Einträge durch Verwendung eines Pseudonyms erreichen.\n# Eine Anonymisierung ist prinzipiell nicht möglich.\n# Dem Nutzer ist bekannt, dass jeder Nutzer die Datenbankinhalte zum Zwecke der Nachvollziehbarkeit herunterladen kann. Eine nachträgliche Pseudonymisierung dieser Daten ist daher nicht möglich.\n# Es ist dem Nutzer aus Gründen des Datenschutzes nicht gestattet, Datenbankinhalte, ganz oder teilweise, Dritten zugänglich zu machen. Ausgenommen hiervon ist eine Weitergabe an Mitglieder der Piratenpartei Deutschland, die diesen Nutzungsbedingungen zustimmen.\n# Der Nutzer darf seine Zugangsdaten nicht an Dritte, auch nicht an andere Piraten, weitergeben. Er darf das System nur selbst persönlich nutzen. Vollmacht zur Stimmübertragung darf ausschließlich systemintern als Delegation erfolgen. Nutzer, die aus persönlichen Gründen das System nicht uneingeschränkt nutzen können, können Ausnahmen beim 1. Beisitzer beantragen."
 
-config.download_use_terms = "Es gelten die Nutzungsbedingungen, insbesondere Punkt 1 ist zu beachten:\n\n"
-
 config.mail_subject_prefix = "[LQFB LSA] "
 
 config.public_access = "anonymous"
@@ -19,4 +17,4 @@ config.issue_discussion_url_func = function(issue)
   return "http://piratenpad.de/" .. string.upper(config.instance_prefix) .. ":LiquidFeedback_Themendiskussion_" .. issue.id
 end
 
--- config.motd_intern = "Auf Anfrage wurde das Regelwerk **Superschnellverfahren** eingerichtet.\n\nEs ist ab sofort im Themenbereich **Sandkasten / Spielwiese** verfügbar. Dieses Regelwerk ist vor allem für Liquid Feedback Schulungen gedacht."
+config.motd_public = "Diese Liquid Feedback Instanz wurde auf die neue Version 2 geupdated. Ab jetzt ist es möglich, E-Mail Benachrichtigungen zu erhalten. Diese müssen in den Einstellungen aktiviert werden.\n\nFalls Anzeigefehler auftreten ist es ratsam den Browser Cache zu leeren (STRG+F5)."
